04bf7a2f8327851b6fa6968992d5e17f02a43e97
Krzystof Kuczewski <k.kuczewski10@gmail.com>
2025-12-05 22:17:05 +0100

    Fixed the formatting of the bluetooth scanner

--- ble-flood.sh
+++ ble-flood.sh
@@ -7,26 +7,36 @@ sleep 2
 sudo rfkill unblock bluetooth # Unlock bluetooth if rfkill disables it (useful for external bluetooth adapters)
 sudo btmgmt --index $INTERFACE_BT power on # Power on the interface
 
-
+echo "\nScanning...\n"
 sudo btmgmt --index $INTERFACE_BT find | tee /tmp/scan.txt # Start the scan
-
 
 # Format everything
 echo -e "\n"
 awk '
 /dev_found/ {
     if (mac) {
-        printf "%-20s %-10s %-5s %-30s\n", mac, type, rssi, (name ? name : "(not available)")
+        printf "%-20s %-10s %-5s %-30s\n", mac, type, rssi, name
     }
-    mac=$2; type=$4; rssi=$6; name="";
+
+    mac = $3;
+    type = $5;
+    name = "(not available)"; # Default name
+    rssi = "N/A";            # Default RSSI
+
+    for (i = 6; i <= NF; i++) {
+        if ($i == "rssi") {
+            rssi = $(i+1);
+            break;
+        }
+    }
 }
 /name/ {
     sub(/^name /, "");
-    name=$0;
+    name = $0;
 }
 END {
     if (mac) {
-        printf "%-20s %-10s %-5s %-30s\n", mac, type, rssi, (name ? name : "(not available)")
+        printf "%-20s %-10s %-5s %-30s\n", mac, type, rssi, name
     }
 }' /tmp/scan.txt
 echo -e "\n\n"
